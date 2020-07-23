f2bb3227b92ace50dc61d107599518228cec235e
EkaRahadi <ekarahadi@gmail.com>
2020-07-23 14:56:41 +0700

    Fix Complaint by Jurusan

--- apiMobile/views.py
+++ apiMobile/views.py
@@ -186,28 +186,40 @@ def listUpdateComplaint(request):
 @csrf_exempt
 @api_view(['GET', 'POST'])
 def listComplaintCategory(request, pk):
-    if int(pk) == 4:
-        if request.data.get("date", None) != None:
-            jurusan = request.data['jurusan']
-            date = request.data['date']
-            month = date[0:2]
-            year = date[3:7]
-            complaint = Complaint.objects.filter(kategori=pk,
-                            tanggal__year=year, 
-                            tanggal__month=month,
-                            jurusan__iexact=jurusan)
-        else:
-            complaint = Complaint.objects.filter(kategori=pk, jurusan__iexact=jurusan)
+    if request.method == 'GET':
+        complaint = Complaint.objects.filter(kategori=pk)
     else:
-        if len(request.data) > 0:
-            date = request.data['date']
-            month = date[0:2]
-            year = date[3:7]
-            complaint = Complaint.objects.filter(kategori=pk,
-                            tanggal__year=year, 
-                            tanggal__month=month)
+        if int(pk) == 4:
+            if request.data.get("jurusan", None) == None:
+                date = request.data['date']
+                month = date[0:2]
+                year = date[3:7]
+                complaint = Complaint.objects.filter(kategori=pk,
+                                tanggal__year=year, 
+                                tanggal__month=month)
+            else:
+                if request.data.get("date", None) != None:
+                    jurusan = request.data['jurusan']
+                    date = request.data['date']
+                    month = date[0:2]
+                    year = date[3:7]
+                    complaint = Complaint.objects.filter(kategori=pk,
+                                    tanggal__year=year, 
+                                    tanggal__month=month,
+                                    jurusan__iexact=jurusan)
+                else:
+                    jurusan = request.data['jurusan']
+                    complaint = Complaint.objects.filter(kategori=pk, jurusan__iexact=jurusan)
         else:
-            complaint = Complaint.objects.filter(kategori=pk)
+            if len(request.data) > 0:
+                date = request.data['date']
+                month = date[0:2]
+                year = date[3:7]
+                complaint = Complaint.objects.filter(kategori=pk,
+                                tanggal__year=year, 
+                                tanggal__month=month)
+            else:
+                complaint = Complaint.objects.filter(kategori=pk)
 
     if len(complaint) > 0 :
         serializer = ComplaintSerializer(complaint, many=True)
